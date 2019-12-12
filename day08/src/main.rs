054061236a69cf582a3c8cca01a10668cb4e10e3
Antonio Alvarez <antonio.alvarez@premium-minds.com>
2019-12-12 14:59:55 +0000

    day8 p2 complete

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -2,7 +2,17 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    println!("{}\n", get_result(find_layer_with_fewest_zero(split_layers((25, 6), to_array(get_input_string())))));
+    print_image(merge_layers(split_layers((25, 6), to_array(get_input_string()))), (25, 6));
+}
+
+fn print_image(image: Vec<char>, size: (i32, i32)){
+    for i in 0..size.1 {
+        for j in 0..size.0 {
+            //print!("{}\n", j + size.0 * i)
+            print!("{}", if image[(j + size.0 * i) as usize] == '0' { ' ' } else { '0' });
+        }
+        print!("\n");
+    }
 }
 
 fn get_result(layer: Vec<char>) -> i32 {
@@ -21,6 +31,19 @@ fn find_layer_with_fewest_zero(layers: Vec<Vec<char>>) -> Vec<char> {
         .clone()
 }
  
+fn merge_layers(layers: Vec<Vec<char>>) -> Vec<char> {
+    (0..layers[0].len())
+        .into_iter()
+        .map(|pos| merge_pixels(layers.iter().map(|layer| layer[pos]).collect()))
+        .collect()
+}
+
+fn merge_pixels(pixels: Vec<char>) -> char {
+    pixels
+        .iter()
+        .fold('2', |acc, pixel| if acc == '2' { pixel.clone() } else { acc })
+}
+
 fn split_layers(size: (i32, i32), input: Vec<char>) -> Vec<Vec<char>> {
     let layer_length = size.0 * size.1;
     input.chunks(layer_length as usize)
